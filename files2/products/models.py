29375d607090873ef3593d00c6513b2f0102b231
Stenardt-9002 <43503237+Stenardt-9002@users.noreply.github.com>
2019-05-14 15:46:46 +0530

    Add files via upload

--- files2/products/models.py
+++ files2/products/models.py
@@ -5,8 +5,7 @@ from django.db import models
 class Product(models.Model):
     """docstring forProduct."""
     title = models.CharField(max_length = 120)
-    description = models.TextField()
-    price = models.TextField()
+    description = models.TextField(blank = True , null = True)
+    price = models.DecimalField(decimal_places = 2, max_digits = 10000)
     summary = models.TextField(default = ' This is cool ')
-
-    # d
+    feature = models.BooleanField()
