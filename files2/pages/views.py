362325f7c8bde8f8d2cc6620f7031adc81b525e9
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2019-05-14 23:50:54 +0530

    Template

--- files2/pages/views.py
+++ files2/pages/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def homepage(*args,**kwargs):
-    return HttpResponse("<h1> Hello world </h1>")
+def homepage(request,*args,**kwargs):
+    # return HttpResponse("<h1> Hello world </h1>")
+    return render(request,"home.html",{})
 
-def contct_view(*arg,**kwargs):
+def contct_view(request,*arg,**kwargs):
     return HttpResponse("<h1> contact page  </h1>")
-    pass
+
 # Create your views here.
+
+def about_view(request):
+    return HttpResponse("<h1> About Page </h1> ")
+
+def social_view(request):
+    return HttpResponse("<h1> Social Page </h1>")
