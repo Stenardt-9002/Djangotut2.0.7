2b5b98c48fef738e8b5e55432951e81a54e07b2f
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2020-09-05 01:50:17 +0530

    Form validation testingand rendering cntrl widget

--- files4/django_react/core/forms.py
+++ files4/django_react/core/forms.py
@@ -3,6 +3,13 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(
+        label='Title',
+        widget=forms.TextInput(attrs={"placeholder": "Your Title"})
+    )
+
+    # override existing tmplate
+
     class Meta:
         model = Product
         fields = [
@@ -10,7 +17,30 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if not "S1" in title:
+            raise forms.ValidationError("This is not a valid title  just add S1 in title")
+        return title
+        pass
+
     pass
 
 
+class RawProductform(forms.Form):
+    title = forms.CharField(required=False)
+    description = forms.CharField(
+        label='',
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Your des",
+                "class": "new-class"
+            }
+        )
+    )
+    price = forms.DecimalField(initial=199.99)
 
+
+pass
--- files4/django_react/core/views.py
+++ files4/django_react/core/views.py
@@ -2,42 +2,40 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from .forms import ProductForm
+from .forms import ProductForm, RawProductform
 from .models import Product
 
 
 def product_create_view(request):
-    context = {}
-
-    return render(request,"core/product_detail.html",context)
-
-
-
-
-
-
-
-
-
-
-
-    # form = ProductForm(request.POST or None)
+    # form = RawProductform()
+    # if request.method == "POST":
+    #     form = RawProductform(request.POST)
+    #     if form.is_valid():
+    #         print(form.cleaned_data)
+    #         Product.objects.create(**form.cleaned_data)
+    #         form = RawProductform()
+    #         pass
+    #     else:
+    #         print(form.errors)
     #
-    # print(request.GET)
-    # print(request.POST)
-    # if form.is_valid():
-    #     print("REACHED ")
-    #     form.save()
-    #     form = ProductForm()
+    # context = {"senor_form": form}
     #
-    #
-    #
-    # context = {
-    #     'form':form
-    # }
-    #
-    # return render(request,"core/product_detail.html",context)
+    # return render(request, "core/product_detail.html", context)
 
+    form = ProductForm(request.POST or None)
+    #
+    print(request.GET)
+    print(request.POST)
+    if form.is_valid():
+        print("REACHED ")
+        form.save()
+        form = ProductForm()
+
+    context = {
+        'senor_form': form
+    }
+
+    return render(request, "core/product_detail.html", context)
 
 # def product_detail_view(request):
 #     obj = Product.objects.get(id = 1)
@@ -46,14 +44,3 @@ def product_create_view(request):
 #     }
 #
 #     return render(request,"products/product_detail.html",context)
-
-
-
-
-
-
-
-
-
-
-
