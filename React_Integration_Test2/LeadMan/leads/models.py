d19d9e3888f638968eb60e270dd548a55f5dcf26
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2021-04-19 12:31:43 +0530

    Integration
    Testing For ML deployment

---
+++ React_Integration_Test2/LeadMan/leads/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Leads(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100,unique=True)
+    message = models.CharField(max_length=500,blank=True)
+    created_At = models.DateTimeField(auto_now_add=True)
+
+
+    pass
+
+
+
+# Create your models here.
+
